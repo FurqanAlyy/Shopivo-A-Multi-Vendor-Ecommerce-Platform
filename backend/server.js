1770df1d84139a3fc163aa912b4153e92b1e4d60
FurqanAlyy <furqan24ali@gmail.com>
2026-09-02 21:44:06 +0500

    errors fixed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
-const express = require('express')
 const dotenv = require('dotenv')
+
+dotenv.config()
+
+const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
@@ -24,8 +27,6 @@ const adminRoutes = require('./routes/adminRoutes')
 const {
   handleStripeWebhook
 } = require('./controllers/paymentController')
-
-dotenv.config()
 
 connectDB()
 
